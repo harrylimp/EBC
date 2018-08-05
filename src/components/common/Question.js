1b7992e3561c3348a63f3d0ae7efed5a1610badb
harrylimp <harrylim.liverpool@gmail.com>
2018-08-05 18:44:09 +1200

    Added re-usable components Question and UserInput. Changed WelcomeScreen.

---
+++ src/components/common/Question.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const Question = ({ question }) => {
+    return (
+        <Text style={styles.questionStyle}>
+            {question}
+        </Text>
+    );
+};
+
+const styles = {
+    questionStyle: {
+        fontSize: 55
+    }
+};
+
+export default Question;
--- src/components/screens/WelcomeScreen.js
+++ src/components/screens/WelcomeScreen.js
@@ -1,10 +1,75 @@
 import React, { Component } from 'react';
 import { View, Image, Text } from 'react-native';
+import Question from '../common/Question';
+import UserInput from '../common/UserInput';
 
 class WelcomeScreen extends Component {
+    state = {
+        question: 'this is the question',
+        answer: 'this is the answer',
+        password: 'real=password',
+        error: 'error with ya know'
+    }
+          
+    onButtonPress() {
+        const { email, password } = this.state;
+
+        this.setState({ error: '', loading: true });
+    }
+
+    onLoginFail() {
+        this.setState({
+            error: 'Authentication Failed',
+            loading: false
+        });
+    }
+
+    onLoginSucess() {
+        this.setState({
+            email: '',
+            password: '',
+            loading: false,
+            error: ''
+        });
+    }
+
+    renderButton() {
+        if (this.state.loading) {
+            return <Spinner size="small" />;
+        }
+
+        return (
+            <Button onPress={this.onButtonPress.bind(this)}>
+                Login
+            </Button>
+        )
+    }
+    
     render() {
         return (
             <View>
+                <Question
+                    question={this.state.question} 
+                />
+                <UserInput
+                    placeHolder='user@gmail.com'
+                    label='Email'
+                    value={this.state.answer}
+                    onChangeText={answer => this.setState({ answer })}
+                />
+                <UserInput
+                    secureTextEntry
+                    placeholder="password"
+                    label="Passowrd"
+                    value={this.state.password}
+                    onChangeText={password => this.setState({ password })}
+                />
+                <Text>
+                    GG
+                </Text> 
+                <Text style={styles.errorTextStyle}>
+                    {this.state.error}
+                </Text>   
                 <Image
                     style={styles.image} 
                     source={{uri: '../../images/UoA_GIF.gif'}}/>
@@ -14,10 +79,11 @@ class WelcomeScreen extends Component {
 }
 
 const styles = {
-    image: {
-        width: 150,
-        height: 150
+    errorTextStyle: {
+      fontSize: 20,
+      alignSelf: 'center',
+      color: 'red'
     }
-}
+};
 
 export default WelcomeScreen;
